cc84e751924789e13c38056828a92d877dfbd318
Gurpreet Singh <gs109690@gmail.com>
2023-05-27 23:24:55 -0500

    Add: Abi and contract address

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -2,6 +2,10 @@ import React from "react"
 import { customMessage, buttonText, ethAmount } from "../../src/custom"
 import Style from "./Main.module.css"
 
+const sendETH = async (params:string) => {
+  
+}
+
 const Main = () => {
   return (
     <div className="w-full h-screen flex flex-col gap-2 justify-center items-center text-center">
@@ -9,7 +13,10 @@ const Main = () => {
       <div className={Style.Input}>
         <div className="flex items-center justify-between  pl-8 mt-4 p-2 py-4 gap-4 bg-white/5 border border-black">
           <input type="text" placeholder={ethAmount} className="" />
-          <button className="bg-black hover:scale-105 text-white  border-white/40 border-[1px] cursor-pointer duration-200 text-lg m-2 ">
+          <button
+            onClick={()=>sendETH}
+            className="bg-black hover:scale-105 text-white  border-white/40 border-[1px] cursor-pointer duration-200 text-lg m-2 "
+          >
             {buttonText}
           </button>
         </div>
--- src/custom.tsx
+++ src/custom.tsx
@@ -6,3 +6,6 @@ export const buttonText: string = "Donate"
 
 // Edit to change input box placeholder.
 export const ethAmount: string = "0"
+
+// Contract address of smart contract.
+export const contractAddress: string = "0xE4aA5Ec56117830114370a5472a3161642C922C3"
