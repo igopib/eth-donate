c766cc98f9fbdea8c6c27e91b399f6a50ce00625
igopib <gs109690@gmail.com>
2023-05-31 12:50:55 -0500

    Add: More inputs for name and message, style later

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -8,6 +8,8 @@ import { parseEther, parseGwei } from "viem"
 import contractInterface from "../../src/contractABI.json"
 
 const Main: React.FC = () => {
+    const [name, setName] = useState<string>("")
+    const [message, setMessage] = useState<string>("")
     const [inputValue, setInputValue] = useState<string>("0")
 
     // Using the Wagmi.sh useContractWrite hook to set up a transaction.
@@ -15,6 +17,7 @@ const Main: React.FC = () => {
         address: "0x8ccaf90ea4f9425d14cb982ace979c542881a147",
         abi: contractInterface,
         functionName: "donateEth",
+        chainId: 11155111,
     })
 
     // Special thanks to chatgpt for this replace and match.
@@ -32,13 +35,21 @@ const Main: React.FC = () => {
         setInputValue(sanitizedValue)
     }
 
+    const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value)
+    }
+
+    const handleMessageChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setMessage(event.target.value)
+    }
+
     // Function runs on click of the button and runs the wagmi write to process the transaction.
     const handleButtonClick = () => {
-        const parsedValue = parseEther(`${parseFloat(inputValue) || 0}`)
+        const parsedValue = parseEther(`${parseFloat(inputValue)}`)
 
-        /* Write() here uses inputs to use pass in donateEth function. parsedValue is used as an parameter for function and also to pass in as value for the payable function */
+        /* Write() here uses inputs to use pass in donateEth function. parsedValue is used as a parameter for the function and also to pass in as value for the payable function */
         write({
-            args: ["Vegita", "It's over 9000!", parsedValue],
+            args: [name, message, parsedValue],
             value: parsedValue,
         })
 
@@ -49,21 +60,43 @@ const Main: React.FC = () => {
         <div className="flex h-screen w-full flex-col items-center justify-center gap-2 text-center">
             <div>{customMessage}</div>
             <div className={Style.Input}>
-                <div className="mt-4 flex items-center  justify-between gap-4 border border-black bg-white/5 p-2 py-4 pl-8">
+                <div className="max-w-[500px] flex-col items-center  gap-4 border border-black bg-white/5">
+                    <input
+                        type="text"
+                        inputMode="text"
+                        placeholder="Your Name"
+                        maxLength={30}
+                        className=""
+                        value={name}
+                        onChange={handleNameChange}
+                    />
+                    <input
+                        type="text"
+                        inputMode="text"
+                        placeholder="Your Message"
+                        maxLength={60}
+                        className=""
+                        value={message}
+                        onChange={handleMessageChange}
+                    />
                     <input
                         type="text"
                         inputMode="decimal"
                         className=""
+                        maxLength={10}
+                        placeholder="0.0"
                         value={inputValue}
                         onChange={handleInputChange}
                     />
-                    <button
-                        disabled={!write}
-                        onClick={handleButtonClick}
-                        className="m-2 cursor-pointer border-[1px]  border-white/40 bg-black text-lg text-white duration-200 hover:scale-105"
-                    >
-                        {buttonText}
-                    </button>
+                    <div className="flex justify-center">
+                        <button
+                            disabled={!write}
+                            onClick={handleButtonClick}
+                            className=" m-4 cursor-pointer items-center justify-center border-[1px] border-white/40 bg-black/90 text-xl text-white duration-200 hover:scale-105"
+                        >
+                            {buttonText}
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
